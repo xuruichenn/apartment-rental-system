94e78a36ba5c79033a1c924a5d7ee4e275486eb2
Yuan-Cheng Tsai <ansonytsai@gmail.com>
2018-04-06 16:06:44 -0700

    Reverted machines back to m4

--- quilt-spec/main.js
+++ quilt-spec/main.js
@@ -5,48 +5,48 @@ const deployment = quilt.createDeployment({namespace: "TEMP", adminACL: ['0.0.0.
 
 var machine0 = new quilt.Machine({
     provider: "Amazon",
-    size: "c4.xlarge",
+    size: "m4.large",
     sshKeys: quilt.githubKeys('TsaiAnson'),
 });
 
 var machine1 = new quilt.Machine({
     provider: "Amazon",
-    size: "c4.xlarge",
+    size: "m4.large",
     sshKeys: quilt.githubKeys('TsaiAnson'),
     diskSize: 15,
 });
 
 var machine2 = new quilt.Machine({
     provider: "Amazon",
-    size: "c4.xlarge",
+    size: "m4.large",
     sshKeys: quilt.githubKeys('TsaiAnson'),
     diskSize: 16,
 });
 
 var machine3 = new quilt.Machine({
     provider: "Amazon",
-    size: "c4.xlarge",
+    size: "m4.large",
     sshKeys: quilt.githubKeys('TsaiAnson'),
     diskSize: 17,
 });
 
 var machine4 = new quilt.Machine({
     provider: "Amazon",
-    size: "c4.xlarge",
+    size: "m4.large",
     sshKeys: quilt.githubKeys('TsaiAnson'),
     diskSize: 18,
 });
 
 var machine5 = new quilt.Machine({
     provider: "Amazon",
-    size: "c4.xlarge",
+    size: "m4.large",
     sshKeys: quilt.githubKeys('TsaiAnson'),
     diskSize: 19,
 });
 
 var machine6 = new quilt.Machine({
     provider: "Amazon",
-    size: "c4.xlarge",
+    size: "m4.large",
     sshKeys: quilt.githubKeys('TsaiAnson'),
     diskSize: 32,
 });
@@ -61,7 +61,7 @@ deployment.deploy(machine2.asWorker());
 deployment.deploy(machine3.asWorker());
 deployment.deploy(machine4.asWorker());
 deployment.deploy(machine5.asWorker());
-deployment.deploy(machine6.asWorker());
+// deployment.deploy(machine6.asWorker());
 
 // Needs to be six machines! (Temporary)
 apartmentApp.machPlacements([15, 16, 17, 18, 19, 32]);
